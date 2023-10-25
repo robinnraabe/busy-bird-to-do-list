0e474d60d46d1c749498901a01fd83eb6464155d
Robin Raabe <robinnraabe@gmail.com>
2023-10-25 03:26:27 -0500

    created database, finished most of the router code

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 Your project description goes here. What problem did you solve? How did you solve it?
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+
+## To Do List To Do List
+  - ✅ Create database
+  - Component to GET tasks (list)
+  - Component to POST new task (form?)
+  - Component to DELETE specific task (item)
+  - Component to PUT task and mark complete (?)
+  - ✅ Set up server side on the router
+  
+  
+  Database requirements:
+  - task text (varchar)
+  - complete (boolean)
+  - due date? (stretch) (sort by?)
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,11 +3,62 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    const queryText = `SELECT * FROM "todo";`;
+    pool.query(queryText)
+        .then((result) => {
+            console.log('GET from server');
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('Error in server GET', error);
+            res.sendStatus(500);
+        });
+})
 
 // POST
+router.post('/', (req, res) => {
+    const task = req.body;
+    const queryText = `
+        INSERT INTO "todo" ("task", "status")
+        VALUES ($1, $2);
+    `;
+    pool.query(queryText, [task.task, task.status])
+        .then((result) => {
+            console.log('POST task to server', task);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log('Error in server POST', error);
+            res.sendStatus(500);
+        });
+})
 
 // PUT
+router.put('/:id', (req, res) => {
+    const queryText = `UPDATE "todo" SET "complete" = true WHERE "id" = $1;`;
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log('Error in server PUT', error);
+            res.sendStatus(500);
+        });
+})
 
 // DELETE
+router.delete('/:id', (req, res) => {
+    console.log('Request to delete id #', req.params.id);
+    const queryText = `DELETE FROM "todo" WHERE "id" = $1;`;
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('Error in server DELETE', error);
+            res.sendStatus(500);
+        });
+})
 
 module.exports = router;
